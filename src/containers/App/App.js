ef2bafd7b13e1bbfc47854dee2acc720604d0e2a
tong4627 <42911309+tong4627@users.noreply.github.com>
2021-01-19 19:36:22 +0800

    user query update

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,62 +7,17 @@ import { Button, Input, Tag } from 'antd'
 
 import {
   USER_QUERY,
-  USERS_QUERY,
   // CREATE_MESSAGE_MUTATION,
   // DELETE_MESSAGE_MUTATION,
   // MESSAGES_SUBSCRIPTION
 } from '../../graphql'
 
 function App() {
-  // const [username, setUsername] = useState('')
-  // const [towhom, setToWhom] = useState('')
-  // const [body, setBody] = useState('')
-
-  // const bodyRef = useRef(null)
-  // const bodyRef2 = useRef(null)
-  var userName = "BadTong";
-  var id = "6006b1c1e511ba463040b9d6";
+  var variables = { userName: "Tien" }
   const { loading, error, data, subscribeToMore } = useQuery(USER_QUERY, {
-    variables: { userName, id}
+    variables: variables
   });
-  // const { loading, error, data, subscribeToMore } = useQuery(USERS_QUERY);
-  // const [addMessage] = useMutation(CREATE_MESSAGE_MUTATION)
 
-  // useEffect(() => {
-  //   subscribeToMore({
-  //     document: MESSAGES_SUBSCRIPTION,
-  //     updateQuery: (prev, { subscriptionData }) => {
-  //       if (!subscriptionData.data) return prev
-  //       const newMessage = subscriptionData.data.message.data
-  //       return {
-  //         ...prev,
-  //         Messages: [...prev.Messages, newMessage]
-  //       }
-  //     }
-  //   })
-  // }, [subscribeToMore])
-  
-  // const handleFormSubmit = useCallback(
-  //   (e) => {
-  //     if (!username || !towhom || !body) return
-
-  //     addMessage({
-  //       variables: {
-  //         from: username,
-  //         to: towhom,
-  //         body: body
-  //       }
-  //     })
-
-  //     setBody('')
-  //   },
-  //   [addMessage, username, towhom, body]
-  // )
-
-  // const clearMessages = () => {
-  //   console.log(data);
-  //   console.log("nothing happen");
-  // }
   const tmp = () => {
     console.log(loading);
     console.log(error);
@@ -72,83 +27,17 @@ function App() {
   return (
     <div>
       <div>hi</div>
-      <Button onClick={tmp} >Testing</Button>
+      <Button onClick={tmp} >Get User Data(in console)</Button>
       <div>
         {loading ? (<div></div>) : ( 
           <div> 
-            <h1> { data.user.id } </h1>
-            <h1> { data.user.userName } </h1>
-            <h1> { data.user.projects } </h1> 
+            <h1> userID: { data.user.id } </h1>
+            <h1> userName: { data.user.userName } </h1>
+            <h1> userProjects:{ data.user.projects[0].projectName } </h1> 
           </div>
         )}
       </div>
     </div>
-  //   <div className="App">
-  //     <div className="App-title">
-  //       <h1>Simple Chat</h1>
-  //       <Button type="primary" danger onClick={clearMessages}>
-  //         Clear
-  //       </Button>
-  //     </div>
-  //     <Input
-  //       placeholder="Username"
-  //       value={username}
-  //       onChange={(e) => setUsername(e.target.value)}
-  //       style={{ marginBottom: 10 }}
-  //       onKeyDown={(e) => {
-  //         if (e.key === 'Enter') {
-  //           bodyRef2.current.focus()
-  //         }
-  //       }}
-  //     ></Input>
-  //     <div className="App-messages">
-  //       {loading ? (
-  //         <p style={{ color: '#ccc' }}>
-  //           { 'Loading...' }
-  //         </p>
-  //       ) : error ? (
-  //         <p style={{ color: '#ccc' }}>
-  //           { 'Error :((((' }
-  //         </p>
-  //       ) : (
-  //         data.Messages.map(({ from, to, body }, i) => {
-  //           return from == username ? (
-  //             <p className="App-message" key={i} align="right">
-  //             {body + "　"}
-  //             <Tag color="blue">{ to }</Tag>
-  //             </p>
-  //           ) : (
-  //             <p className="App-message" key={i} align="left">
-  //             <Tag color="red">{ from }</Tag>
-  //             {body}
-  //           </p>
-  //           )})    
-  //       )}
-  //     </div>
-
-
-  //     <Input
-  //       placeholder="ToWhom"
-  //       value={towhom}
-  //       ref={bodyRef2}
-  //       onChange={(e) => setToWhom(e.target.value)}
-  //       style={{ marginBottom: 10 }}
-  //       onKeyDown={(e) => {
-  //         if (e.key === 'Enter') {
-  //           bodyRef.current.focus()
-  //         }
-  //       }}
-  //     ></Input>
-  //     <Input.Search
-  //       rows={4}
-  //       value={body}
-  //       ref={bodyRef}
-  //       enterButton="Send"
-  //       onChange={(e) => setBody(e.target.value)}
-  //       placeholder="Type a message here..."
-  //       onSearch={handleFormSubmit}
-  //     ></Input.Search>
-  //   </div>
   )
 }
 
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,27 +1,5 @@
 import { gql } from 'apollo-boost'
 
-// export const USER_PROJECTS_QUERY = gql`
-//   query user(
-//       $userName: String
-//       $id: ID
-//     ) {
-//       user(
-//         userName: $userName
-//         id: $ID
-//       ) {
-//         id
-//         projects {
-//           id
-//           projectName
-//           assignments {
-//             id
-//             assignmentName
-//           }
-//         }
-//       }
-//   }
-// `
-
 const USER_QUERY = gql`
   query user(
     $userName: String
@@ -36,22 +14,26 @@ const USER_QUERY = gql`
         projects {
           id
           projectName
+          assignments {
+            id
+            assignmentName
+          }
         }
       }
   }
 `
 
-const USERS_QUERY = gql`
-  query {
-    users {
-      id
-      userName
-      projects {
-        id
-        projectName
-      }
-    }
-  }
-`
+// const USERS_QUERY = gql`
+//   query {
+//     users {
+//       id
+//       userName
+//       projects {
+//         id
+//         projectName
+//       }
+//     }
+//   }
+// `
 
-export {USER_QUERY, USERS_QUERY};
+export {USER_QUERY};
