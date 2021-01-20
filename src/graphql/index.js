950606cbe3be2f634b61bc4da2fe53b311f9bba7
jasper0314-huang <b07501122@ntu.edu.tw>
2021-01-21 02:40:30 +0800

    update

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,6 +2,7 @@ export { USER_QUERY } from './queries'
 export { 
     CREATE_ASSIGNMENT_MUTATION, 
     UPDATE_ASSIGNMENT_MUTATION, 
-    DELETE_ASSIGNMENT_MUTATION 
+    DELETE_ASSIGNMENT_MUTATION,
+    CREATE_RECORD_MUTATION
 } from './mutations'
 export { USER_SUBSCRIPTION } from './subscriptions'
