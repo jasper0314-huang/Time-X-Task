cd86e3ec674700785b5a337057089fd326c575ed
eeeric <erictien02@gmail.com>
2021-01-20 23:39:38 +0800

    update statistic query

--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -77,9 +77,75 @@ const Query = {
     })
     return result
   },
-  // async projectNames(parent, args, content, info) {
-  //   var result
-  // }
+  async projectNames(parent, args, content, info) {
+    var name_set = new Set();
+    var projects = await Project.find();
+    projects.forEach( (element) => {
+      name_set.add(element.projectName)
+    })
+    var project_names = [];
+    for (let name_i of name_set) {
+      project_names.push({ name: name_i, count: 1 })
+    }
+    return project_names
+  },
+  async assignmentNames(parent, { projectName }, content, info) {
+    var name_set = new Set();
+    let projects = await Project.find({
+      "projectName": projectName
+    }).populate({
+      path: 'assignments',
+      model: 'assignment',
+    })
+    for (let project_i of projects) {
+      project_i.assignments.forEach( (ele) => {
+        name_set.add(ele.assignmentName)
+      } )
+    }
+    var assignment_names = [];
+    for (let name_i of name_set) {
+      assignment_names.push({ name: name_i, count: 1 })
+    }
+    return assignment_names
+  },
+  async searchStats(parent, args, content, info) {
+    let time_array = [];
+    let projects = await Project.find({
+      "projectName": args.projectName
+    }).populate({
+      path: 'assignments',
+      model: 'assignment',
+      populate: {
+        path: 'records',
+        model: 'record'
+      }
+    })
+
+    if (args.assignmentName) {
+      for (let project_i of projects) {
+        for (let assignment_i of project_i.assignments) {
+          let time_sum = 0;
+          if (assignment_i.assignmentName === args.assignmentName) {
+            for (let record_i of assignment_i.records) {
+              time_sum = record_i.duration + time_sum
+            }
+          }
+          time_array.push(time_sum)
+        }
+      }
+    } else {
+      for (let project_i of projects) {
+        let proj_time_sum = 0;
+        for (let assignment_i of project_i.assignments) {
+          for (let record_i of assignment_i.records) {
+            proj_time_sum = record_i.duration + proj_time_sum
+          }
+        }
+        time_array.push(proj_time_sum)
+      }
+    }
+    return time_array
+  }
 }
 
 module.exports = Query
